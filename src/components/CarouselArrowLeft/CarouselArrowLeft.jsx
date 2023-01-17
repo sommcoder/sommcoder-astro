9df31aba5f7855ee75f29710ed4a911b75e1b3ad
Brian Davies <brian.davies589@gmail.com>
2023-01-17 15:56:42 -0500

    changin back to regular css modules

--- src/components/CarouselArrowLeft/CarouselArrowLeft.jsx
+++ src/components/CarouselArrowLeft/CarouselArrowLeft.jsx
@@ -1,23 +1,5 @@
-﻿import styled from "styled-components";
+﻿import "./CarouselArrowLeft.css";
 
 export default function CarouselArrowLeft() {
-  return <StyledCarouselArrowLeft></StyledCarouselArrowLeft>;
+  return <div className="carousel-arrow-left"></div>;
 }
-const StyledCarouselArrowLeft = styled.div`
-  display: none;
-
-  @media (min-width: 1280) {
-    display: inline-block;
-    width: 0;
-    height: 0;
-    border-top: 10px solid transparent;
-    border-right: 10px solid white;
-    border-bottom: 10px solid transparent;
-
-    margin-left: 1.5rem;
-
-    &:hover {
-      cursor: pointer;
-    }
-  }
-`;
